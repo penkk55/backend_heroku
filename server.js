18bb9aaa15614aeef736c2ecdedbbe1e47798fa8
Pongparn Charernburanopas <penkk55@gmail.com>
2022-06-12 22:59:55 +0700

    API LOGIN สำเร็จ

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express")
+// const { json } = require("express/lib/response")
 const app = express()
 const PORT = process.env.PORT || 8081
 // let mysql = require('mysql');
+
+let bodyParser = require('body-parser');
+
+//jwt
+// Set up Global configuration access
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+dotenv.config();
+
 
 
 // let dbCon = mysql.createConnection({
@@ -15,11 +25,19 @@ const PORT = process.env.PORT || 8081
 //     res.json({result: "ok", data:[1,2,3,4,5]})
 // })
 
-app.get("/", (req, res)=>{
+// body parse middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended:false}))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/:aa", (req, res)=>{
   res.json({
-    username: "godwqdqwdqwdqw",
-    paswword: "666"
+    username: "god",
+    password: "666"
   })
+  // res.send('test' + req.params.aa)
 
 })
 app.use((req, res, next)=>{
@@ -33,15 +51,35 @@ app.use((req, res, next)=>{
 app.post("/api/login", (req, res)=>{
   let username = req.body.username
   let password = req.body.password
-
-  if(username || password) {
-    return res.json({
-      username: "god",
-      paswword: "666"
+  // res.send(req.body)
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  let data = {
+    time: Date(),
+    userId: 12,
+}
+  const token = jwt.sign(data, jwtSecretKey);
+  
+  if(!username || !password) {
+    return res.status(400).send({ 
+      status: "error",
+      message: "Login failed"
+    });
+  }
+  else {
+    return res.send({
+      status: "ok",
+      message: "Logged in",
+      accessToken: token,
+      user : {
+        username: req.body.username,
+        password: req.body.password
+      }
+      
     })
   }
-  
 })
+  
+
 
 app.listen(PORT,()=>{
   console.log(`server is running${PORT}`)
