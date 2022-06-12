a35ce242b28337af065d3586aa2dcc1ca018f684
Pongparn Charernburanopas <penkk55@gmail.com>
2022-06-12 17:48:41 +0700

    add God user

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8081
+// let mysql = require('mysql');
 
+
+// let dbCon = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root999',
+//   database: 'nodejs_api'
+// })
+
+// app.get("/", (req, res)=>{
+//     res.json({result: "ok", data:[1,2,3,4,5]})
+// })
 
 app.get("/", (req, res)=>{
-    res.json({result: "ok", data:[1,2,3,4,5]})
+  res.json({
+    username: "god",
+    paswword: "666"
+  })
+
+})
+
+// app.put("/api/login", (req, res)=>{
+//   res.json({result: "ok", data:[1,2,3,4,5]})
+// })
+app.post("/api/login", (req, res)=>{
+  res.json({
+    username: "god",
+    paswword: "666"
+  })
 })
 
 app.listen(PORT,()=>{
